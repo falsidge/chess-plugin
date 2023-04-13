5fc90205a6e79c5dff6e94cf5dda9a8649c46b86
falsidge <tesetings@gmail.com>
2023-04-12 18:22:22 -0700

    init

--- main.py
+++ main.py
@@ -4,33 +4,82 @@ import quart
 import quart_cors
 from quart import request
 
+import aiohttp
+import chess
+
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
-
 # Keep track of todo's. Does not persist if Python session is restarted.
-_TODOS = {}
-
-@app.post("/todos/<string:username>")
-async def add_todo(username):
+_CHESS = {}
+@app.post("/chess/board/<string:username>")
+async def load_board(username):
     request = await quart.request.get_json(force=True)
-    if username not in _TODOS:
-        _TODOS[username] = []
-    _TODOS[username].append(request["todo"])
-    return quart.Response(response='OK', status=200)
-
-@app.get("/todos/<string:username>")
-async def get_todos(username):
-    return quart.Response(response=json.dumps(_TODOS.get(username, [])), status=200)
-
-@app.delete("/todos/<string:username>")
-async def delete_todo(username):
+    FEN = request.get("fen", "")
+    print(FEN)
+    _CHESS[username] = chess.Board(FEN)
+    return quart.Response(response="OK", status=200)
+@app.get("/chess/board/<string:username>")
+async def get_board(username):
+    board = _CHESS[username] = _CHESS.get(username, chess.Board())
+    data = {"fen":board.fen()}
+    return quart.Response(response=json.dumps(data), status=200)
+@app.post("/chess/make_move/<string:username>")
+async def make_move(username):
     request = await quart.request.get_json(force=True)
-    todo_idx = request["todo_idx"]
-    # fail silently, it's a simple plugin
-    if 0 <= todo_idx < len(_TODOS[username]):
-        _TODOS[username].pop(todo_idx)
-    return quart.Response(response='OK', status=200)
 
-@app.get("/logo.png")
+    move = request.get("move", "")
+    print("move", move)
+    try:
+        board = _CHESS[username] = _CHESS.get(username, chess.Board())
+        board.push_san(move)
+    except ValueError:
+        return quart.Response(response="Invalid move", status=400)
+    data = {"fen":board.fen()}
+    return quart.Response(response=json.dumps(data), status=200)
+@app.get("/chess/valid_moves/<string:username>")
+async def valid_moves(username):
+
+
+    board = _CHESS[username] = _CHESS.get(username, chess.Board())
+
+    data = {"moves":list(map(lambda x:x.uci(), board.generate_legal_moves()))}
+    return quart.Response(response=json.dumps(data), status=200)
+        
+@app.get("/chess/analysis/<string:username>")
+async def get_analysis(username):
+    request = await quart.request.get_json(force=True)
+    board = _CHESS[username] = _CHESS.get(username, chess.Board())
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://lichess.org/api/cloud-eval',params={"fen":board.fen()}) as response:
+            data = await response.json()
+            return quart.Response(response=json.dumps(data), status=200)
+        
+@app.post("/chess/analysis")
+async def post_analysis():
+    request = await quart.request.get_json(force=True)
+    FEN = request.get("fen", "")
+    print(FEN)
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://lichess.org/api/cloud-eval',params={"fen":FEN}) as response:
+            data = await response.json()
+            return quart.Response(response=json.dumps(data), status=200)
+
+@app.post("/chess/display")
+async def post_display():
+    request = await quart.request.get_json(force=True)
+    FEN = request.get("fen", "")
+    print(FEN)
+    chessboard = chess.Board(FEN)
+    data = {"display":chessboard.unicode()}
+    return quart.Response(response=json.dumps(data), status=200)
+@app.get("/chess/display/<string:username>")
+async def get_display(username):
+    request = await quart.request.get_json(force=True)
+    board = _CHESS[username] = _CHESS.get(username, chess.Board())
+
+    data = {"display":board.unicode()}
+    return quart.Response(response=json.dumps(data), status=200)
+
+@app.get("/chess.png")
 async def plugin_logo():
     filename = 'logo.png'
     return await quart.send_file(filename, mimetype='image/png')
