20b1dcbf2a9aae5e7422c3fd68ae0a30e57744ac
falsidge <tesetings@gmail.com>
2023-04-13 08:03:16 -0700

    Add images

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import urllib.parse
 import json
 
 import quart
@@ -6,10 +7,15 @@ from quart import request
 
 import aiohttp
 import chess
+import chess.svg
 
-app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
-# Keep track of todo's. Does not persist if Python session is restarted.
+import cairosvg
+
+app = quart_cors.cors(quart.Quart(__name__),
+                      allow_origin="https://chat.openai.com")
 _CHESS = {}
+
+
 @app.post("/chess/board/<string:username>")
 async def load_board(username):
     request = await quart.request.get_json(force=True)
@@ -20,11 +26,15 @@ async def load_board(username):
     else:
         _CHESS[username] = chess.Board(FEN)
     return quart.Response(response="OK", status=200)
+
+
 @app.get("/chess/board/<string:username>")
 async def get_board(username):
     board = _CHESS[username] = _CHESS.get(username, chess.Board())
-    data = {"fen":board.fen()}
+    data = {"fen": board.fen()}
     return quart.Response(response=json.dumps(data), status=200)
+
+
 @app.post("/chess/make_move/<string:username>")
 async def make_move(username):
     request = await quart.request.get_json(force=True)
@@ -36,34 +46,39 @@ async def make_move(username):
         board.push_san(move)
     except ValueError:
         return quart.Response(response="Invalid move", status=400)
-    data = {"fen":board.fen()}
+    data = {"fen": board.fen()}
     return quart.Response(response=json.dumps(data), status=200)
+
+
 @app.get("/chess/valid_moves/<string:username>")
 async def valid_moves(username):
 
-
     board = _CHESS[username] = _CHESS.get(username, chess.Board())
 
-    data = {"moves":list(map(lambda x:x.uci(), board.generate_legal_moves()))}
+    data = {"moves": list(
+        map(lambda x: x.uci(), board.generate_legal_moves()))}
     return quart.Response(response=json.dumps(data), status=200)
-        
+
+
 @app.get("/chess/analysis/<string:username>")
 async def get_analysis(username):
     board = _CHESS[username] = _CHESS.get(username, chess.Board())
     async with aiohttp.ClientSession() as session:
-        async with session.get('https://lichess.org/api/cloud-eval',params={"fen":board.fen()}) as response:
+        async with session.get('https://lichess.org/api/cloud-eval', params={"fen": board.fen()}) as response:
             data = await response.json()
             return quart.Response(response=json.dumps(data), status=200)
-        
+
+
 @app.post("/chess/analysis")
 async def post_analysis():
     request = await quart.request.get_json(force=True)
     FEN = request.get("fen", "")
     print(FEN)
     async with aiohttp.ClientSession() as session:
-        async with session.get('https://lichess.org/api/cloud-eval',params={"fen":FEN}) as response:
+        async with session.get('https://lichess.org/api/cloud-eval', params={"fen": FEN}) as response:
             data = await response.json()
             return quart.Response(response=json.dumps(data), status=200)
+
 
 @app.post("/chess/display")
 async def post_display():
@@ -71,19 +86,52 @@ async def post_display():
     FEN = request.get("fen", "")
     print(FEN)
     chessboard = chess.Board(FEN)
-    data = {"display":chessboard.unicode()}
+    url = app.url_for(
+        'get_png_fen', fen=urllib.parse.quote_plus(FEN),  _external=True)
+
+    data = {"display": {
+        "png": f"[Chessboard]({url}", "ascii": chessboard.unicode(), }}
     return quart.Response(response=json.dumps(data), status=200)
+
+
 @app.get("/chess/display/<string:username>")
 async def get_display(username):
-    board = _CHESS[username] = _CHESS.get(username, chess.Board())
+    chessboard = _CHESS[username] = _CHESS.get(username, chess.Board())
 
-    data = {"display":board.unicode()}
+    url = app.url_for('get_png_fen', fen=urllib.parse.quote_plus(
+        chessboard.fen()),  _external=True)
+    data = {"display": {
+        "png": f"[Chessboard]({url})", "ascii": chessboard.unicode(), }}
+
     return quart.Response(response=json.dumps(data), status=200)
+
+
+@app.get("/chess/svg/<string:username>.svg")
+async def get_svg(username):
+    chessboard = _CHESS[username] = _CHESS.get(username, chess.Board())
+
+    return quart.Response(response=chess.svg.board(chessboard), status=200, mimetype="img/svg+xml")
+
+
+@app.get("/chess/png/<string:username>.png")
+async def get_png(username):
+    chessboard = _CHESS[username] = _CHESS.get(username, chess.Board())
+
+    return quart.Response(response=cairosvg.svg2png(chess.svg.board(chessboard)), status=200, mimetype="img/svg+xml")
+
+
+@app.get("/chess/png/fen/<string:fen>.png")
+async def get_png_fen(fen):
+    chessboard = chess.Board(urllib.parse.unquote_plus(fen))
+
+    return quart.Response(response=cairosvg.svg2png(chess.svg.board(chessboard)), status=200, mimetype="img/svg+xml")
+
 
 @app.get("/chess.png")
 async def plugin_logo():
     filename = 'logo.png'
     return await quart.send_file(filename, mimetype='image/png')
+
 
 @app.get("/.well-known/ai-plugin.json")
 async def plugin_manifest():
@@ -92,6 +140,7 @@ async def plugin_manifest():
         text = f.read()
         return quart.Response(text, mimetype="text/json")
 
+
 @app.get("/openapi.yaml")
 async def openapi_spec():
     host = request.headers['Host']
@@ -99,8 +148,10 @@ async def openapi_spec():
         text = f.read()
         return quart.Response(text, mimetype="text/yaml")
 
+
 def main():
     app.run(debug=True, host="0.0.0.0", port=5003)
+
 
 if __name__ == "__main__":
     main()
